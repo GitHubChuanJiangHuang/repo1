ef1e4a1e456d4c47bc96d219a3e95bd51cb192d5
hcj <1798943069@qq.com>
2019-10-23 19:45:30 +0800

    晚上试试

--- print_demo/src/com/xgn/demo1/GetSum.java
+++ print_demo/src/com/xgn/demo1/GetSum.java
@@ -3,7 +3,7 @@ package com.xgn.demo1;
 public class GetSum {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 10; i++) {
             sum += i;
         }
         System.out.println("和为："+sum);
