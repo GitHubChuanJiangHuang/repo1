99531036bb27a85ab5c1a052d7bfa4a2b4baa38a
shieq <shieq@nzsoft.com>
2019-10-23 14:46:47 +0800

    测试1

---
+++ print_demo/src/com/xgn/demo1/printShu.java
@@ -0,0 +1,9 @@
+package com.xgn.demo1;
+
+public class printShu {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("超级棒！！"+"  "+i);
+        }
+    }
+}
